b77e87c1fa5dab4b6dee7163c6c95ac201fa20df
Montana Scher <amscher@gmail.com>
2015-04-13 14:15:07 -0700

    updated script for csvs

--- ccoll-csv-convert.py
+++ ccoll-csv-convert.py
@@ -2,14 +2,13 @@ import Sentence
 from os import listdir
 from os.path import isfile, join, isdir
 
-for dir in listdir("RS_PATH.62"):
-  # print dir[0:4]
-  if not isdir("RS_PATH.62/" + dir):
+for dir in listdir("../RS_PATH.62"):
+  if not isdir("../RS_PATH.62/" + dir):
     continue
   patient_id = dir[0:12]
-  if isfile("RS_PATH.62/" + dir + "/input.text"):
+  if isfile("../RS_PATH.62/" + dir + "/input.text"):
     print patient_id
-    input = open("RS_PATH.62/" + dir + "/input.text", 'r')
+    input = open("../RS_PATH.62/" + dir + "/input.text", 'r')
     output = open("csv_output/" + patient_id + ".csv", 'w')
     line = input.readline()
     sent = Sentence.Sentence(patient_id, patient_id+"SENT_1")
@@ -21,6 +20,7 @@ for dir in listdir("RS_PATH.62"):
           sent = Sentence.Sentence(patient_id, patient_id+tokens[7])
         sent.words.append(tokens[1])
         sent.lemma.append(tokens[4])
+        sent.dep_tags.append(tokens[6])
         sent.pos_tags.append(tokens[2])
       line = input.readline()
     output.write(sent.to_string() + "\n")
